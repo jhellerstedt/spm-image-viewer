f9d8fcd2f4d908cb31d94fcd94eca2707e4eb866
Jack Hellerstedt <hellerstedt.jack@gmail.com>
2017-07-10 14:38:56 +0200

    added killing server pid multiple ports

--- server_lifecycle.py
+++ server_lifecycle.py
@@ -7,13 +7,28 @@ Created on Mon Dec 19 17:29:28 2016
 """
 
 import core_functions as cf
-from bokeh.io import curdoc
+
+import os
+import signal
+import pickle
 
 def on_server_loaded(server_context):
     pass
     
 def on_server_unloaded(server_context):
     pass
-    
-def on_session_destroyed(session_context):
+
+def on_session_created(session_context):
     pass
+
+def on_session_destroyed(session_context):
+    try:
+        open_ports = pickle.load(open_ports.p)
+        port = bokeh.Server.port
+        open_ports.remote(port)
+        pickle.dump(open_ports, open_ports.p)
+    except:
+        print("port not excised from open_ports")
+    pid = os.getpid()
+    os.kill(pid, signal.SIGTERM)
+    return
